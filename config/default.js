33c01cc3c00ca3aecb6d584dcbacd90b027bc31b
tormozz48 <tormozz48@gmail.com>
2017-11-29 19:34:46 +0300

    Add application configuration file

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+    application: {
+        port: 3000
+    },
+    db: {
+        host: 'localhost', //database host
+        port: 5432, //database port
+        user: 'postgres', //postgres user
+        password: undefined, //postgres password
+        database: 'easypay' //database name
+    }
+};
